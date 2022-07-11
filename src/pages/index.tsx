08779904f4b1ce733f020eac21f91e65f2ffb90a
ViniciusItalo <vinicius.italo@kultivi.com>
2022-07-10 21:24:06 -0300

    feat: initial structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Fragment } from "react";
+
+const Home: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>SYS Finance | Transções em total segurança.</title>
+      </Head>
+    </Fragment>
+  );
+};
+
+export default Home;
